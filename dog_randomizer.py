1c0b5826ad657017faeb69026ffaa0690d08b063
Leonard Kuan <leonkyy7@gmail.com>
2023-02-26 17:17:29 +0000

    initial commit to create repo

---
+++ dog_randomizer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 17:08:55 2023
+
+@author: Leonard
+"""
+
+from tkinter import *
+window=Tk()
+# add widgets here
+
+btn=Button(window, text="This is Button widget", fg='blue')
+btn.place(x=80, y=100)
+lbl=Label(window, text="This is Label widget", fg='red', font=("Helvetica", 16))
+lbl.place(x=60, y=50)
+txtfld=Entry(window, text="This is Entry Widget", bd=5)
+txtfld.place(x=80, y=150)
+
+window.title('Hello Python')
+window.geometry("300x200+10+20")
+window.mainloop()
+
